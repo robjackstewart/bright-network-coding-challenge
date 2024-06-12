af06bad16d75bde00e33876c0bd55f5dde434771
Rob Jack Stewart <robjackstewart@gmail.com>
2024-06-12 11:31:04 +0000

    consider common meaningful words without geos

--- src/application/queries/get_opportunities.py
+++ src/application/queries/get_opportunities.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
+import re
+from typing import Tuple
 
-import spacy
+import nltk
 from src.application.common.interfaces.interfaces import (
     JobsRepositoryInterface,
     MembersRepositoryInterface,
@@ -18,22 +20,43 @@ class GetOpportunitiesQuery:
         self._members_repository = members_repository
 
     async def execute(self) -> dict[Member, list[Job]]:
+        nltk.download('averaged_perceptron_tagger', quiet=True)
+        nltk.download('punkt', quiet=True)
+
         jobs = await self._jobs_repository.jobs()
         members = await self._members_repository.members()
         jobs_by_members = {}
 
+        locations = set([job.location.lower() for job in jobs])
+
         for member in members:
             jobs_for_member = []
-            lower_case_bio = member.bio.lower()
-            bio_words = set(lower_case_bio.split())
+            lower_bio = GetOpportunitiesQuery.__strip_non_alphanumeric_chars(
+                member.bio.lower())
+            bio_tokens = GetOpportunitiesQuery.__get_tokens(lower_bio)
+            meaningful_bio_words = set(
+                GetOpportunitiesQuery.__extract_meaningful_words(bio_tokens))
             for job in jobs:
-                job_title_words = set(job.title.lower().split())
-                common_words_between_bio_and_job_title = bio_words.intersection(job_title_words)
-                if len(common_words_between_bio_and_job_title) > 0:
-                    jobs_for_member.append(job)
+                lower_case_job_title = job.title.lower()
+                for word in meaningful_bio_words:
+                    if word not in locations and word in lower_case_job_title and job not in jobs_for_member:
+                        jobs_for_member.append(job)
+            
+            
+
             jobs_by_members[member] = jobs_for_member
 
         return jobs_by_members
+
+    def __get_tokens(subject: str) -> list[Tuple[str, str]]:
+        tokens = nltk.word_tokenize(subject)
+        return nltk.pos_tag(tokens)
+
+    def __extract_meaningful_words(tokens: list[Tuple[str, str]]):
+        return [word for word, pos in tokens if pos in ['NN', 'NNS', 'NNP', 'NNPS', 'JJ', 'JJR', 'JJS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'] and len(word) > 1]
+
+    def __strip_non_alphanumeric_chars(subject: str):
+        return re.sub(r'[^a-zA-Z0-9 ]', '', subject)
 
     # def __get_geographies(self, subject: str):
     #     result = self._spacy(subject)
