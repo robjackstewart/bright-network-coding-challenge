d9631114e1fdd2d926b36ec3fc575bee3ee1366d
Rob Jack Stewart <robjackstewart@gmail.com>
2024-06-12 13:08:07 +0000

    formatting

--- src/application/queries/get_opportunities.py
+++ src/application/queries/get_opportunities.py
@@ -37,32 +37,49 @@ class GetOpportunitiesQuery:
         for member in members:
             jobs_for_member = []
             lower_bio = GetOpportunitiesQuery.__strip_non_alphanumeric_chars(
-                member.bio.lower())
-            meaningful_bio_words = [word for word in GetOpportunitiesQuery.__strip_non_alphanumeric_chars(
-                lower_bio).split() if len(word) > 3]
-            preferred_geographies: list[str] = GetOpportunitiesQuery.__calculate_preferred_geographies(
-                lower_bio, locations)
+                member.bio.lower()
+            )
+            meaningful_bio_words = [
+                word
+                for word in GetOpportunitiesQuery.__strip_non_alphanumeric_chars(
+                    lower_bio
+                ).split()
+                if len(word) > 3
+            ]
+            preferred_geographies: list[str] = (
+                GetOpportunitiesQuery.__calculate_preferred_geographies(
+                    lower_bio, locations
+                )
+            )
             jobs_to_consider_for_member_based_on_geography: list[Job] = []
             if len(preferred_geographies) > 0:
                 for preferred_geopraphy in preferred_geographies:
                     jobs_to_consider_for_member_based_on_geography.extend(
-                        jobs_by_location[preferred_geopraphy])
+                        jobs_by_location[preferred_geopraphy]
+                    )
             else:
                 jobs_to_consider_for_member_based_on_geography = jobs
             for job in jobs_to_consider_for_member_based_on_geography:
                 lower_case_job_title = job.title.lower()
                 for word in meaningful_bio_words:
-                    if word not in locations and word in lower_case_job_title and job not in jobs_for_member:
+                    if (
+                        word not in locations
+                        and word in lower_case_job_title
+                        and job not in jobs_for_member
+                    ):
                         jobs_for_member.append(job)
 
             jobs_by_members[member] = set(jobs_for_member)
 
         return jobs_by_members
 
-    def __calculate_preferred_geographies(bio: str, available_geographies: list[str] = []) -> Optional[list[str]]:
+    def __calculate_preferred_geographies(
+        bio: str, available_geographies: list[str] = []
+    ) -> Optional[list[str]]:
         locations_in_bio = [
-            location for location in available_geographies if location in bio.lower()]
+            location for location in available_geographies if location in bio.lower()
+        ]
         return locations_in_bio
 
     def __strip_non_alphanumeric_chars(subject: str):
-        return re.sub(r'[^a-zA-Z0-9 ]', '', subject)
+        return re.sub(r"[^a-zA-Z0-9 ]", "", subject)
--- tests/application/queries/test_get_opportunities.py
+++ tests/application/queries/test_get_opportunities.py
@@ -17,22 +17,21 @@ async def test_execute_returns_all_jobs_for_all_members():
     marta = Member(name="Marta", bio="I'm looking for an internship in London")
     hassan = Member(name="Hassan", bio="I'm looking for a design job")
     grace = Member(
-        name="Grace", bio="I'm looking for a job in marketing outside of London")
+        name="Grace", bio="I'm looking for a job in marketing outside of London"
+    )
     daisy = Member(
-        name="Daisy", bio="I'm a software developer currently in Edinburgh but looking to relocate to London")
+        name="Daisy",
+        bio="I'm a software developer currently in Edinburgh but looking to relocate to London",
+    )
 
-    london_software_developer = Job(
-        title="Software Developer", location="London")
-    york_marketing_internship = Job(
-        title="Marketing Internship", location="York")
+    london_software_developer = Job(title="Software Developer", location="London")
+    york_marketing_internship = Job(title="Marketing Internship", location="York")
     london_data_scientist = Job(title="Data Scientist", location="London")
     london_legal_internship = Job(title="Legal Internship", location="London")
-    manchester_project_manager = Job(
-        title="Project Manager", location="Manchester")
+    manchester_project_manager = Job(title="Project Manager", location="Manchester")
     london_sales_internship = Job(title="Sales Internship", location="London")
     london_ux_designer = Job(title="UX Designer", location="London")
-    edinburgh_software_developer = Job(
-        title="Software Developer", location="Edinburgh")
+    edinburgh_software_developer = Job(title="Software Developer", location="Edinburgh")
 
     jobs: list[Job] = [
         london_software_developer,
@@ -42,28 +41,21 @@ async def test_execute_returns_all_jobs_for_all_members():
         manchester_project_manager,
         london_sales_internship,
         london_ux_designer,
-        edinburgh_software_developer
+        edinburgh_software_developer,
     ]
 
-    members: list[Member] = [
-        joe,
-        marta,
-        hassan,
-        grace,
-        daisy
-    ]
+    members: list[Member] = [joe, marta, hassan, grace, daisy]
 
     expected = {
         joe: set([london_ux_designer]),
         marta: set([london_legal_internship, london_sales_internship]),
         hassan: set([london_ux_designer]),
         grace: set([]),
-        daisy: set([london_software_developer, edinburgh_software_developer])
+        daisy: set([london_software_developer, edinburgh_software_developer]),
     }
     jobs_repository = create_autospec(JobsRepositoryInterface, instance=True)
     jobs_repository.jobs.return_value = jobs
-    members_repository = create_autospec(
-        MembersRepositoryInterface, instance=True)
+    members_repository = create_autospec(MembersRepositoryInterface, instance=True)
     members_repository.members.return_value = members
     query = GetOpportunitiesQuery(jobs_repository, members_repository)
 
