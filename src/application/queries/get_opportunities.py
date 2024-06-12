0734695dc32d2cb24fc47c3c5a0e318a09d71c8e
Rob Jack Stewart <robjackstewart@gmail.com>
2024-06-12 13:09:29 +0000

    remove package

--- src/application/queries/get_opportunities.py
+++ src/application/queries/get_opportunities.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 import re
-from typing import Optional, Tuple
-
-import nltk
+from typing import Optional
 from src.application.common.interfaces.interfaces import (
     JobsRepositoryInterface,
     MembersRepositoryInterface,
